657dc0329ad667bc75eb2c088dc2a54d22d8d46b
lenyoun <132416519+lenyoun@users.noreply.github.com>
2023-06-27 22:35:02 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -5,17 +5,59 @@ import os
 
 os.system("sudo pip3 install torch")
 os.system("sudo pip3 install torchvision")
+os.system("sudo pip3 install tqdm")
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
-
+import cillections
+import numpy as np
+from tqdm import tqdm
 from torch.utils.data import DataLoader
     
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+classes = ('airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+cfg = {
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+}
 
 class NeuralNetwork(nn.Module):
-    pass
+    class NeuralNetwork(nn.Module):
+    def __init__(self, vgg_name):
+        super(NeuralNetwork, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+LR = 0.001
+EPOCHS = 20
+BATCHSIZE = 100
 
 def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
